c5117ec3da7e676a26eb569ed4aac5c894f335a3
Cheederah <misscheederah@gmail.com>
2018-04-29 07:44:37 +0100

    my first

---
+++ index.php
@@ -0,0 +1,48 @@
+ <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Hotels.ng</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="CSS/style.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <header>
+        <h1>Hotels.ng Internship pRoGrAm</h1>
+    </header>
+    <div>
+        <p>
+           <?php
+           date_default_timezone_set("Africa/Lagos");
+               echo date("h:ia");
+           ?>
+           
+        </p> 
+    </div>
+    <nav>
+        <p>"It never stops ticking". </p>
+    </nav>
+
+
+    <article class="contenteditable">
+        <p>"Time waits for no man".</p>
+       <p> Register now for <span>hotels.ng</span> internship program </p>
+       <article class="s"> simply click on Register</article>
+      <a href="https://hng.fun/"> 
+      <button type="submit" class="registerbtn">Register</button>
+      </a>
+
+</article>
+<aside class="sign">
+<p>Already have an account? <a href="#">Sign in</a>.</p>
+</aside>
+
+    <footer>
+        <p> &copy; Copyright 2018 Cheederah</p>
+
+        </footer>
+
+</body>
+</html>
